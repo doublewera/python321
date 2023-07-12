402b99e5a81d19ce96f86041a58f7e54b17208c4
Wera Barinova <alisawera@gmail.com>
2023-07-12 19:53:26 +0300

    Задача 4: корзина товаров и итог цены

--- goods.py
+++ goods.py
@@ -6,3 +6,24 @@ goods = {
     'лук': 15,
     'морковка': 40
 }
+
+left_col = 15
+
+# Двойная подстановка!
+
+print('+' + '-' * left_col + '+' + '-' * left_col + '+')
+
+print('|%(name)15s' % {'name': 'лук'} + '|', end='')
+print('%(price)15i' % {
+    'price': goods['лук']
+    } + '|')
+
+print('+' + '-' * left_col + '+' + '-' * left_col + '+')
+print('|' + 'морковка' + '|')
+print('+' + '-' * left_col + '+' + '-' * left_col + '+')
+print('|' + 'гречка' + '|')
+print('+' + '-' * left_col + '+' + '-' * left_col + '+')
+print('| Итог     |' + str(
+     goods['лук'] +  goods['гречка'] +  goods['морковка']
+    ) + '|')
+print('+' + '-' * left_col + '+' + '-' * left_col + '+')
